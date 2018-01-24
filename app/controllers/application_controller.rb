c9f260d2f38c66a6f37e25586f13f577cf623741
Romain Bousseau <romansubbi@gmail.com>
2018-01-24 18:49:05 +0100

    app controller with devise & after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Pundit
 
+  # Devise
+  before_action :authenticate_user!, unless: :devise_controller?
+
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
-  def after_sign_in_path(resource)
+  def after_sign_in_path_for(resource)
     users_games_path
   end
 
