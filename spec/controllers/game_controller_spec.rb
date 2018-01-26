41f86599d89fbab0871f5d03cc5d97a18054718e
Romain Bousseau <romansubbi@gmail.com>
2018-01-26 13:26:21 +0100

    start a test controller

--- spec/controllers/game_controller_spec.rb
+++ spec/controllers/game_controller_spec.rb
@@ -1,9 +1,13 @@
-# Needs FIXING !!!!
-describe Users::GamesController do
+require 'spec_helper'
+include Devise::Test::ControllerHelpers
+
+describe Users::GamesController, :type => :controller do
+  login_user
+
   describe "GET index" do
      it "renders the index template" do
-      get :index
-      expect(response).to render_template(“index”)
+      get users_games_url
+      render_template(“index”)
     end
   end
 end
